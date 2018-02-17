f0a53eecdd2707c11399acb9dc008de57f9994f6
Jaydeep Rotithor <rotithoj@flip3.engr.oregonstate.edu>
2018-02-16 17:27:25 -0800

    got charts and web map working

--- web_map/app.js
+++ web_map/app.js
@@ -17,20 +17,11 @@ app.use(express.static(__dirname ));
 
     io.sockets.on('connection', function (socket) {
 
-        var JSON_len = data_subsets.features.length;
-        for(i = 0; i < JSON_len; i++) {
-            socket.emit('join', "HJANDREWS: " +  data_subsets.features[i].properties.Field1);
-            socket.emit('join2', "IDTAG: " +  data_subsets.features[i].properties.Field2);
-            socket.emit('join3', "TIMESTAMP: " +  data_subsets.features[i].properties.Field3);
-            socket.emit('join4', "TEMPC: " +  data_subsets.features[i].properties.Field4);
-            socket.emit('join5', "HUMID: " +  data_subsets.features[i].properties.Field5);
-            socket.emit('join6', "LOADCELL: " +  data_subsets.features[i].properties.Field6);
-            socket.emit('join7', "IRLIGHT: " +  data_subsets.features[i].properties.Field7);
-            socket.emit('join8', "FULLLIGHT: " +  data_subsets.features[i].properties.Field8);
-            socket.emit('join9', "BATVOLT: " +  data_subsets.features[i].properties.Field9);
+
+            socket.emit('join',  data_subsets);
 
 
-        }
+
     });
 
 
