27214eb65e32187706033e740730d250abf85539
jrotithor <rotithoj@oregonstate.edu>
2018-03-20 23:24:11 -0700

    put python as background process

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var http = require("http");
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
-
+var spawn = require("child_process").spawn;
+var p = spawn("python", ["py2csv.py"], {detached: true});
 
 
 
